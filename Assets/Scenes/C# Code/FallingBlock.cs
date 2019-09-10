1ec652fe8c1e10c277b6716e2d4fc3b436889520
vedantatrivedi <42843768+vedantatrivedi@users.noreply.github.com>
2019-09-10 19:30:13 +0530

    Initial

---
+++ Assets/Scenes/C# Code/FallingBlock.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingBlock : MonoBehaviour
+{
+    float speed = 7;
+    // Start is called before the first frame update
+    // void Start()
+    // {
+    
+    // }
+
+    // Update is called once per frame
+    void Update()
+    {
+            transform.Translate(Vector3.down * speed * Time.deltaTime);
+        
+    }
+}
